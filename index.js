fe541df51f0bc527c456a4f13174b2aa945f5d34
Bisho silwal <aayussilwal@gmail.com>
2024-07-28 14:01:42 -0500

    install helmet for security

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 require("./src/data/db");
-
+const helmet = require("helmet");
 const express = require("express");
 
 const dishRouter = require("./src/routes/dishRoutes");
@@ -8,6 +8,7 @@ const ingredientRouter = require("./src/routes/ingredientRoutes");
 
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
