04ae359cc67443f1521ed2170ee2cab9c268874a
Bisho silwal <aayussilwal@gmail.com>
2024-07-28 10:20:36 -0500

    added 404 error

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/dishes", dishRouter);
 app.use("/api/ingredients", ingredientRouter);
 
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Not found" });
+});
+
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
