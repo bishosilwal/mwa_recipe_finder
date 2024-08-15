8d5dc27ad5a8a9b91361e1a1370b3403a5c6449c
Bisho silwal <aayussilwal@gmail.com>
2024-08-15 11:13:56 -0500

    added jwt token

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,8 @@
 const User = require("../models/User");
 var bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+
+const secretKey = process.env.JWT_SECRET_KEY;
 
 const createUser = async (req, res) => {
   try {
@@ -45,9 +48,12 @@ const loginUser = async (req, res) => {
         return res.status(500).json({ message: err.message });
       }
       if (result) {
+        var token = jwt.sign({ email: user.email }, secretKey, {
+          expiresIn: "1d",
+        });
         res.status(200).json({
           message: "Login successful",
-          user: user,
+          token: token,
         });
       } else {
         res.status(401).json({ message: "Invalid credentials" });
